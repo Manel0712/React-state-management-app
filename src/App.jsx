33d4f82ffaf97b8b18e4d21c05ef30f6917cb3b2
Manel De la Hoz <mdelahoz@bytemaster.es>
2024-02-08 10:07:36 +0100

    Complet 1.6-1.11 Exercies

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,6 @@
 import { useState } from "react";
+import { StatisticLine } from "./StatisticLine";
+import { Button } from "./Button";
 
 const App = () => {
   // save each button click in its own state
@@ -6,7 +8,49 @@ const App = () => {
   const [neutral, setNeutral] = useState(0);
   const [bad, setBad] = useState(0);
 
-  return <div>code here</div>;
+  const handleIncreaseGood = () => {
+    setGood((prevGood) => prevGood + 1);
+  };
+
+  const handleIncreaseNeutral = () => {
+    setNeutral((prevNeutral) => prevNeutral + 1);
+  };
+
+  const handleIncreaseBad = () => {
+    setBad((prevBad) => prevBad + 1);
+  };
+
+  const totalFeedback = good + neutral + bad;
+
+  const averageScore =  (good - bad) / totalFeedback;
+
+  const positivePercentage = (good * 100) / totalFeedback
+
+  return ( <div>
+  <h1>give feedback</h1>
+  <Button onClick={handleIncreaseGood} text="good" />
+  <Button onClick={handleIncreaseNeutral} text="neutral" />
+  <Button onClick={handleIncreaseBad} text="bad" />
+  <h1>statistics</h1>
+  {totalFeedback === 0 ? (
+    <p>No feedback given</p>
+  ) : (
+    <table>
+      <tbody>
+        <StatisticLine text="good" value={good} />
+        <StatisticLine text="neutral" value={neutral} />
+        <StatisticLine text="bad" value={bad} />
+        <StatisticLine text="all" value={totalFeedback} />
+        <StatisticLine text="average" value={averageScore} />
+        <StatisticLine
+          text="positive"
+          value={positivePercentage + "%"}
+        />
+      </tbody>
+    </table>
+  )}
+</div>
+  )
 };
 
 export default App;
