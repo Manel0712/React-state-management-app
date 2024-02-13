1ed75f656691fc962291f7869cb886a01aacf7a3
Manel De la Hoz <mdelahoz@bytemaster.es>
2024-02-13 11:42:28 +0100

    Complet 1.12-1.14 Exercies

--- src/App.jsx
+++ src/App.jsx
@@ -24,7 +24,42 @@ const App = () => {
 
   const averageScore =  (good - bad) / totalFeedback;
 
-  const positivePercentage = (good * 100) / totalFeedback
+  const positivePercentage = (good * 100) / totalFeedback;
+
+  const anecdotes = [
+    'If it hurts, do it more often.',
+    'Adding manpower to a late software project makes it later!',
+    'The first 90 percent of the code accounts for the first 90 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.',
+    'Any fool can write code that a computer can understand. Good programmers write code that humans can understand.',
+    'Premature optimization is the root of all evil.',
+    'Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.',
+    'Programming without an extremely heavy use of console.log is same as if a doctor would refuse to use x-rays or blood tests when diagnosing patients.',
+    'The only way to go fast, is to go well.'
+  ]
+   
+  const [selected, setSelected] = useState(0);
+  const [text, setText] = useState("If it hurts, do it more often");
+  const [votes, setVotes] = useState(new Array(anecdotes.length).fill(0));
+
+  const handleSelect = () => {
+    const newSelected = Math.floor(Math.random() * anecdotes.length);
+    setSelected(newSelected);
+    setText(anecdotes[newSelected]);
+  }
+
+  const handleVoteAnecdote = () => {
+    const newVotes = [...votes];
+    newVotes[selected]++;
+    setVotes(newVotes);
+  }
+
+  const maxVotes = Math.max(...votes);
+  const maxIndices = votes.reduce((acc, vote, index) => {
+    if (vote === maxVotes) {
+      acc.push(index);
+    }
+    return acc;
+  }, []);
 
   return ( <div>
   <h1>give feedback</h1>
@@ -49,6 +84,13 @@ const App = () => {
       </tbody>
     </table>
   )}
+  <p>{text}</p>
+  <p>has {votes[selected]} votes</p>
+  <button onClick={handleVoteAnecdote}>vote</button>
+  <button onClick={handleSelect}>next anecdote</button>
+  <h1>Anecdote with the most votes</h1>
+      <p>{anecdotes[maxIndices[0]]}</p>
+      <p>has {maxVotes} votes</p>
 </div>
   )
 };
